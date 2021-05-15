f95780dc70e72d5192b3227f1d7c96b90da656f5
manhloi5501 <manhloi0505@gmail.com>
2021-05-15 22:29:24 +0700

    connect to oracle db

--- App/WeddingManagementApp/src/application/Main.java
+++ App/WeddingManagementApp/src/application/Main.java
@@ -1,21 +1,31 @@
 package application;
 	
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	public static Connection connection;
+	
+	public void initDatabase() throws ClassNotFoundException, SQLException {
+		connection = ConnectDB.getOracleConnection();
+	}
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = (VBox)FXMLLoader.load(getClass().getResource("indexUI.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("loginUI.fxml"));
 			Scene scene = new Scene(root,1120,681);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			initDatabase();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
