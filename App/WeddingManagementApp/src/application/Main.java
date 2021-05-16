d8937091f7b82fc965416502c1211c6bb8b41e96
nguyenphuc1040 <19520851@gm.uit.edu.vn>
2021-05-16 11:57:09 +0700

    update interface controller

--- App/WeddingManagementApp/src/application/Main.java
+++ App/WeddingManagementApp/src/application/Main.java
@@ -6,10 +6,8 @@ import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
-
 
 public class Main extends Application {
 	public static Connection connection;
@@ -21,7 +19,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("loginUI.fxml"));
-			Scene scene = new Scene(root,1120,681);
+			Scene scene = new Scene(root,637,454);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -29,9 +27,11 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+		
 	}
 }
