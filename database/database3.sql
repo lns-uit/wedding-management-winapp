49ed340358912c5866ead5d94a51188067d8a3c7
Your name here <khoitr190@gmail.com>
2021-05-20 12:43:36 +0700

    write procedure Lobby insert update delete

--- database/database3.sql
+++ database/database3.sql
@@ -57,3 +57,86 @@ begin
         V_return := 'false';
     end if;
 end;
+
+insert into customer (nameCustomer) values(
+    'Vô Danh'
+);
+
+create or replace trigger deleteCustomer
+before delete ON Customer
+for each row
+begin
+    update OrderWedding set idCustomer = 'Cus2' where idcustomer = :old.idcustomer;
+    update Bill set idCustomer = 'CUS2' where idcustomer = :old.idcustomer;
+end;
+
+create or replace procedure sp_insertLobby(
+    V_NAMELOBBY in Lobby.NAMELOBBY%type,
+    V_MAXTABLE in Lobby.MAXTABLE%type,
+    V_PRICETABLE in Lobby.PRICETABLE%type,
+    V_PRICELOBBY in Lobby.PRICELOBBY%type,
+    V_NOTE in Lobby.NOTE%type,
+    V_LOBBYTYPE in Lobby.LOBBYTYPE%type,
+    V_return out varchar2
+)
+is  
+    lobbyBeforeInsert number(4);
+    lobbyAfterInsert number(4);
+begin
+    select count(*) into lobbyBeforeInsert from Lobby;
+    insert into Lobby(NAMELOBBY,MAXTABLE,PRICETABLE,PRICELOBBY,NOTE,LOBBYTYPE) values(
+        V_NAMELOBBY,V_MAXTABLE,V_PRICETABLE,V_PRICELOBBY,V_NOTE,V_LOBBYTYPE
+    );
+    select count(*) into lobbyAfterInsert from Lobby;
+    if lobbyAfterInsert > lobbyBeforeInsert then
+        V_return := 'true';
+    else 
+        V_return := 'false';
+    end if;
+end;
+
+create or replace procedure sp_UpdateLobby(
+    V_idLobby in Lobby.idLobby%type,
+    V_NAMELOBBY in Lobby.NAMELOBBY%type,
+    V_MAXTABLE in Lobby.MAXTABLE%type,
+    V_PRICETABLE in Lobby.PRICETABLE%type,
+    V_PRICELOBBY in Lobby.PRICELOBBY%type,
+    V_NOTE in Lobby.NOTE%type,
+    V_LOBBYTYPE in Lobby.LOBBYTYPE%type,
+    V_return out varchar2
+)
+is LobbyTmp number(1);
+begin
+    update Lobby set
+        NAMELOBBY = V_NAMELOBBY,
+        MAXTABLE = V_MAXTABLE,
+        PRICETABLE = V_PRICETABLE,
+        PRICELOBBY = V_PRICELOBBY,
+        NOTE = V_NOTE,
+        LOBBYTYPE = V_LOBBYTYPE
+    where idLobby = V_idLobby;
+    select count(*) into LobbyTmp from Lobby
+    where idLobby = V_idLobby and NAMELOBBY = V_NAMELOBBY and MAXTABLE = V_MAXTABLE and PRICETABLE = V_PRICETABLE 
+    and PRICELOBBY = V_PRICELOBBY and NOTE = V_NOTE and LOBBYTYPE = V_LOBBYTYPE;
+    if LobbyTmp = 1 then
+        V_return := 'true';
+    else
+        V_return := 'false';
+    end if;
+end;
+
+create or replace procedure sp_deleteLobby(
+    V_idLobby in Lobby.idLobby%type,
+    V_note in Lobby.note%type,
+    V_return out varchar2
+)
+is lobbyTmp number(1);
+begin
+    update Lobby set note = V_note  where V_idLobby = idLobby;
+    select count(*) into lobbyTmp from Lobby where V_idLobby = idLobby and note = V_note  ;
+    if lobbyTmp = 1 then
+        V_return := 'true';
+    else
+        V_return := 'false';
+    end if;
+end;
